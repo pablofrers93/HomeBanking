27d1b40cf00a4d99ff75cbfae3ae989879df101d
pablofrers93 <pablofrers93@gmail.com>
2023-07-24 23:17:30 -0300

    creación de repo

---
+++ HomeBankingMinHub/HomeBankingMinHub/Models/HomeBankingContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HomeBankingMinHub.Models
+{
+    public class HomeBankingContext:DbContext
+    {
+        public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) { }
+    }
+}

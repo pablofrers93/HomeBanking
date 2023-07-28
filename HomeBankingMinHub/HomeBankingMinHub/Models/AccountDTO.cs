b299819e5aa618fc2fb390b716e62918dc614f57
pablofrers93 <pablofrers93@gmail.com>
2023-07-27 23:23:03 -0300

    Implements accounts

---
+++ HomeBankingMinHub/HomeBankingMinHub/Models/AccountDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeBankingMinHub.Models
+{
+    public class AccountDTO
+    {
+        public long Id { get; set; }
+        public string Number { get; set; }
+        public DateTime CreationDate { get; set; }
+        public double Balance { get; set; }
+
+    }
+}
